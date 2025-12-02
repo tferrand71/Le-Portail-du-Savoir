a9822b7f5baeab6bb408edbdc989d6d406fdde38
tferrand71 <tobiasferrand6@gmail.com>
2025-12-02 23:33:59 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // <--- On importe HashRouter
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter> {/* <--- On utilise HashRouter ici */}
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Remplacez 'NOM_DE_VOTRE_REPO' par le nom exact de votre dépôt GitHub (ex: 'portail-savoir')
 export default defineConfig({
     plugins: [react()],
+    base: '/Le-Portail-du-Savoir/',
 })
